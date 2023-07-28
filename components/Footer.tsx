c7667792d8c2ddc7ea8500086d5a66c88566b453
demah <hamedsedighi@gmail.com>
2023-07-28 21:00:20 +0200

    add github icon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,18 @@
+import Link from 'next/link'
+import GithubIcon from './Icons/GithubIcon'
+
 const Footer = () => (
-  <div className="flex flex-col items-center justify-center py-20">
+  <div className="flex flex-col items-center justify-center gap-3 py-20">
     <h6 className="primary-text-color font-sans text-lg font-bold">Kartly</h6>
     <small>Online shopping center</small>
+    <Link
+      className="flex w-fit cursor-pointer gap-3 rounded-md border border-black bg-black fill-white p-2 text-white transition-all duration-300 hover:bg-white hover:fill-black hover:text-black"
+      href="https://github.com/herol3oy/kartly"
+      target="_blank"
+    >
+      <GithubIcon />
+      <h6>Github</h6>
+    </Link>
   </div>
 )
 
